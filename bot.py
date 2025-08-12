8441e2e19b145337955174d6abe97ba738ff10e5
Harshit Chourasia <chouurasiaharshit42@gmail.com>
2025-08-12 23:04:36 +0530

    Added DB_PORT to MySQL connection

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
@@ -23,6 +24,7 @@ DB_NAME = os.getenv("DB_NAME")
 # Connect to MySQL
 db = mysql.connector.connect(
     host=DB_HOST,
+    port=int(os.environ.get("DB_PORT", 3306)),
     user=DB_USER,
     password=DB_PASSWORD,
     database=DB_NAME
